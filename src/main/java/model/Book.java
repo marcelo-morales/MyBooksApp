8b8ffcb5af9f7598c28b1636dbe22f962f530f21
marcelomorales <lmoral10@jhu.edu>
2020-12-31 16:28:28 -0500

    added a comment line for feature B

--- src/main/java/model/Book.java
+++ src/main/java/model/Book.java
@@ -9,6 +9,7 @@ public class Book {
     private int year;
     private String author;
 
+    //This only shows on featureB Branch
     public Book(String title, String isbn, String publisher, int year, String author) {
         this.title = title;
         this.isbn = isbn;
